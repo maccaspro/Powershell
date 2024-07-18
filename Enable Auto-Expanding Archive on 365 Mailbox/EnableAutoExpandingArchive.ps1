c4bf750e5c62e68b7769bdb6dcf02131f8226123
Matt <55187116+maccaspro@users.noreply.github.com>
2024-07-18 22:42:35 +0100

    Update EnableAutoExpandingArchive.ps1

--- Enable Auto-Expanding Archive on 365 Mailbox/EnableAutoExpandingArchive.ps1
+++ Enable Auto-Expanding Archive on 365 Mailbox/EnableAutoExpandingArchive.ps1
@@ -1,4 +1,11 @@
 enable-mailbox user@domain.com -autoexpandingarchive
+#for enabling on an individual user's mailbox
+
+Set-OrganizationConfig -AutoExpandingArchive
+#enables for entire Org
 
 get-mailbox user@domain.com |fl autoexpandingarchiveenabled
-#verify Auto Expanding Archive has been enabled on the mailbox
+#verify Auto Expanding Archive has been enabled on the individual mailbox
+
+Get-OrganizationConfig | FL AutoExpandingArchiveEnabled
+#verify it has been enabled for the org
