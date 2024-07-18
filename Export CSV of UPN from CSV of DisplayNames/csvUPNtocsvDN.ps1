d02999567d5bd9f386f851c5da0ddff8b62ed49e
Matt <55187116+maccaspro@users.noreply.github.com>
2024-07-18 18:44:52 +0100

    Create csvUPNtocsvDN.ps1

---
+++ Export CSV of UPN from CSV of DisplayNames/csvUPNtocsvDN.ps1
@@ -0,0 +1,9 @@
+#Displayname CSV should have column titled "User"
+
+$users=@()
+$usercsv = import-csv "c:\temppath\displaynames.csv"
+foreach($user in $UserCSV)
+{
+         $users += Get-AzureADUser -SearchString $user.User | Select UserPrincipalName
+ }
+$users| export-csv "c:\temppath\UPNs.csv" -notypeinformation
