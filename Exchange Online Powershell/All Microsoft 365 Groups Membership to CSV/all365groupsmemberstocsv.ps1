318f2a50e43ef19aa82150132abdf80f0bcb9b6c
Matt <55187116+maccaspro@users.noreply.github.com>
2024-08-05 11:50:13 +0100

    Update all365groupsmemberstocsv.ps1

--- Exchange Online Powershell/All Microsoft 365 Groups Membership to CSV/all365groupsmemberstocsv.ps1
+++ Exchange Online Powershell/All Microsoft 365 Groups Membership to CSV/all365groupsmemberstocsv.ps1
@@ -3,4 +3,4 @@ $groups |ForEach-Object { $groupId = $_.GroupId
 >> $GroupName = $_.DisplayName
 >> $members = Get-UnifiedGroupLinks -Identity $groupname -LinkType members
 >> $members |select-object @{Name="Group";Expression={$groupName}},@{Name="User";Expression={$_.displayname}}
->> } | Export-csv -path "C:\TempPath\CHR Group Members.csv" -NoTypeInformation
+>> } | Export-csv -path "C:\TempPath\365 Group Members.csv" -NoTypeInformation
