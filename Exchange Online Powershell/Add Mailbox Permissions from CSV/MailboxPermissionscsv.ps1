fa2bd391f69180ebeb7d98ca3043dd7f18bd308c
Matt <55187116+maccaspro@users.noreply.github.com>
2024-10-03 08:52:40 +0100

    Update MailboxPermissionscsv.ps1

--- Exchange Online Powershell/Add Mailbox Permissions from CSV/MailboxPermissionscsv.ps1
+++ Exchange Online Powershell/Add Mailbox Permissions from CSV/MailboxPermissionscsv.ps1
@@ -1,5 +1,5 @@
 import-csv "c:\temppath\Displaynames.csv"|foreach {Add-MailboxPermission mailbox@domain.com -user $_.Displayname -AccessRights FullAccess}
 #Adds read permissions from CSV of displaynames with column titled "displayname"
 
-import-csv "c:\temppath\newcastle homework mailbox.csv"|foreach {Add-RecipientPermission newcastlehomework@vhg.co.uk -AccessRights SendAs -trustee $_.Displayname -confirm:$false}
+import-csv "c:\temppath\Displaynames.csv"|foreach {Add-RecipientPermission mailbox@domain.com -AccessRights SendAs -trustee $_.Displayname -confirm:$false}
 #Adds SendAs permissoins from CSV of displaynames with column titled "displayname"
