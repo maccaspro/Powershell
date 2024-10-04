3d1538b7e57fb82be8e8b94c6bbf9f3036d3aa9e
Matt <55187116+maccaspro@users.noreply.github.com>
2024-10-04 11:56:45 +0100

    Create FolderPermissions.ps1

---
+++ Local Powershell/Extract Folder Permissions/FolderPermissions.ps1
@@ -0,0 +1,31 @@
+# Set path
+$FolderPath = Get-ChildItem -Directory -path "\\mtcentserver\sysvol\Database" -Recurse -Force
+
+$Output = @()
+
+ForEach ($Folder in $FolderPath) {
+    $Acl = Get-Acl -Path $Folder.FullName
+
+    
+    ForEach ($Access in $Acl.Access) {
+        $Properties = [ordered]@{
+        "Name" = $Folder.Name
+        "Group/User" = $Access.IdentityReference
+        "Type" = $Access.AccessControlType
+        "Permissions" = $Access.FileSystemRights
+        "Inherited" = $Access.IsInherited
+        "Inheritance" = $Access.InheritanceFlags
+        "Propagation" = $Access.PropagationFlags
+        "Location" = $Folder.FullName
+        }
+
+$Output += New-Object -TypeName PSObject -Property $Properties            
+    }
+}
+
+$date = (Get-Date).ToString('yyyy-MM-dd_HH-mm')
+$fileName = "ACL-List-" + $date + ".csv"
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+$fullPath = $DesktopPath + "\" + $fileName
+ 
+$Output | Export-Csv -Path $fullPath -NoTypeInformation
