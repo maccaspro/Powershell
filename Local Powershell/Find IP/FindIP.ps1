ed115f7cd207c6a881046d94975e9487e3ddc635
Matt <55187116+maccaspro@users.noreply.github.com>
2024-11-04 14:21:52 +0000

    Update FindIP.ps1

--- Local Powershell/Find IP/FindIP.ps1
+++ Local Powershell/Find IP/FindIP.ps1
@@ -1,2 +1,8 @@
 $Net = Get-NetIPAddress -addressfamily ipv4
-$Net|Where-Object IPAddress -notlike "169.254.*"|Where-Object IPAddress -notmatch "127.0.0.1"|ft ipaddress,interfacealias
+$ip = $net|Where-Object IPAddress -notlike "169.254.*"|Where-Object IPAddress -notmatch "127.0.0.1"
+
+if($ip.IPAddress -like "10.0.*"){
+    write-host "On local network"
+}else {
+    write-host "Not on local network"
+}
