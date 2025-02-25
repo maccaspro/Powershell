3d8a999e25bb2ba1bb98e42df4cbe225ebf37555
Matt <55187116+maccaspro@users.noreply.github.com>
2025-02-25 09:20:33 +0000

    Update WindowsUpdateRefresh.ps1

--- Local Powershell/Windows Update Fix/WindowsUpdateRefresh.ps1
+++ Local Powershell/Windows Update Fix/WindowsUpdateRefresh.ps1
@@ -1,4 +1,4 @@
-stop-service wuauserv, bits, cryptsvc, msiserver
+stop-service wuauserv, bits, cryptsvc, msiserver -f
 rm -r -fo C:\windows\SoftwareDistribution
 rm -r -fo C:\windows\System32\catroot2
 rm -r -fo C:\windows\System32\appmgmt
