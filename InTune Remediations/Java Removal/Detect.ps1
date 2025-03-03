a091da540e52242ddd99bb7920a6213763ab7e5f
Matt <55187116+maccaspro@users.noreply.github.com>
2025-03-03 16:39:40 +0000

    Update Detect.ps1

--- InTune Remediations/Java Removal/Detect.ps1
+++ InTune Remediations/Java Removal/Detect.ps1
@@ -1,6 +1,7 @@
 $path = "C:\Program Files (x86)\Java"
+$path2 = "C:\Program Files\Java"
 
-if(Test-Path -path $path){
+if((Test-Path $path) -or (Test-Path $path2)){
     Write-Warning "Not Compliant"
     Exit 1
 }
